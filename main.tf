d56af682ebf320019b6de6e4f966993153e175a5
lipi-ops <raylipi@gmail.com>
2025-11-11 12:07:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-rovider "aws" {
+provider "aws" {
   region     = "ap-south-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0305d3d91b9f22e84" # Amazon Linux 2
+  ami           = "ami-03695d52f0d883f65" # Amazon Linux 2
   instance_type = "t2.micro"
   tags = {
     Name = "GitHubActionsEC2"
   }
 }
+
