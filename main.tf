ca63b6e9b4d8ea508b1292231518c449de4ef4a4
lipi-ops <raylipi@gmail.com>
2025-11-12 15:17:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-ami-03695d52f0d883f65" # Amazon Linux 2
+  instance_type = "t2.micro"
+  tags = {
+    Name = "GitHubActionsEC2"
+  }
+}
