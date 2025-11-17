1e59680dcfea71a93adde1bc5824506dcbfba5f8
lipi-ops <raylipi@gmail.com>
2025-11-17 18:40:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
- "terraform": {
-        "backend": {
-            "s3": {
-                "bucket": "terraform-lipi-test",
-                "key": "mydoc/terraform.tfstate",
-                "region": "ap-south-1"
-            }
-        }
-    }
-
+ terraform {
+  backend "s3" {
+    bucket         = "terraform-lipi-test"   # your S3 bucket name
+    key            = "mydoc/terraform.tfstate"       # path inside the bucket
+    region         = "ap-south-1"                  # AWS region
+    encrypt        = true
+  }
+}
