7e4d84043252147af3bff8e04a4837edf945d7c2
lipi-ops <raylipi@gmail.com>
2025-11-17 16:44:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region     = "ap-south-1"
-  --access_key = var.aws_access_key
- -- secret_key = var.aws_secret_key
-}
+  }
 
 resource "aws_instance" "example" {
   ami           = "ami-03695d52f0d883f65" # Amazon Linux 2
